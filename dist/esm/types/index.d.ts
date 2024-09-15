32037d22bea382a389c0f74a69e427f237003d5d
miryoku7 <84623421+miryoku7@users.noreply.github.com>
2024-09-16 02:46:53 +0900

    refactor: Improve book data extraction logic in fetchDetail/book.ts

--- dist/esm/types/index.d.ts
+++ dist/esm/types/index.d.ts
@@ -3,13 +3,14 @@ export type Year = 2020 | 2021 | 2022 | 2023 | 2024;
 export type Field = "教養学部" | "教育学部" | "経済学部" | "理学部" | "工学部" | "教育機構";
 export type FetchList = (options: Options) => Promise<LessonSummary[]>;
 export type FetchDetail = (id: string, lang?: "1" | "2" | "ja" | "en" | "日" | "英") => Promise<LessonDetail>;
+export type LessonId = string;
 export type Options = {
     year: Year;
     field: Field;
 };
 export type LessonSummary = {
     numberings: string;
-    id: string;
+    id: LessonId;
     language: string;
     lct_cd: string;
     je_cd: string;
