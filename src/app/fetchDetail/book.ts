88212c2c328a6ebe629832b6f5ea2735afda0d7f
miryoku7 <84623421+miryoku7@users.noreply.github.com>
2024-09-16 02:45:43 +0900

    refactor: Improve book data extraction logic in fetchDetail/book.ts

--- src/app/fetchDetail/book.ts
+++ src/app/fetchDetail/book.ts
@@ -1,34 +1,40 @@
 import { load } from "cheerio";
 import { type Book } from "@/types";
 
-
 const book = (html: string) => {
   const $ = load(html);
+
   const data: Book[] = [];
-
-  $("a").each((i, elem) => {
-    const href = $(elem).attr("href");
-    // start with https://opac.lib.saitama-u.ac.jp/Main/OpenSearch?isbn=
-    if (href && href.startsWith("https://opac.lib.saitama-u.ac.jp/Main/OpenSearch?isbn=")) {
-
-      const title = $(elem).text().trim();
+  
+  // id が "itemBook_" で始まり "_lblIsbn" で終わる span 要素を選択
+  const spans = $('span[id^="ctl00_phContents_ucSylContents"][id$="_lblIsbn"]');
+  spans.each((i, elem) => {
+    // CheerioでHTMLをロード
+    const isbnBase = $(elem).text().trim();
+    
+    if (isbnBase) {
       const type = $(elem).closest("tbody").find("td").eq(0).text().trim() || "";
+      const title = $(elem).closest("tbody").find("td:nth-child(2)").eq(1).text().trim() || "";
       if (title === "") {
         // やめる
         return;
       }
 
-      const isbnBase = href.replace("https://opac.lib.saitama-u.ac.jp/Main/OpenSearch?isbn=", "");
       // isbnからハイフンを除去
       const isbn = isbnBase.replace(/-/g, "");
-
+      // isbnが数字で構成されているかチェック
+      if (!/^\d+$/.test(isbn)) {
+        // やめる
+        return;
+      }
       data.push({
         isbn,
         title,
         bookType: type as Book["bookType"],
       });
     }
-  }) 
+
+  })
 
   return data;
 }
